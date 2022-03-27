90ababa2678e15892263e231d14129efd06a9e68
ttse23 <terence.tse23@gmail.com>
2022-03-27 01:16:02 -0700

    Complete 08_calculator Exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, num) => sum += num, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prod, num) => prod *= num, 1)
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+	return Math.pow(n1, n2)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n < 0){
+    return undefined
+  } else if (n === 0){
+    return 1
+  } else{
+    return n * factorial(n - 1)
+  }
 };
 
 // Do not edit below this line
