688e96e8f0a8a716a701b6468de4816a69167737
ttse23 <terence.tse23@gmail.com>
2022-03-27 01:49:05 -0700

    Complete 10_fibonacci Exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(targetIndex) {
+    targetIndex = Number(targetIndex)
+    if (targetIndex < 0){
+        return "OOPS"
+    }
+    let arr = [1, 1]
+    for (let i = 2; i <= targetIndex; i++){
+        arr.push(arr[i - 2] + arr[i - 1])
+    }
+    return arr[targetIndex  - 1]
 };
 
 // Do not edit below this line
