6d137ca8b812a4f2b823f9abe1c0aa136aba4dae
ttse23 <terence.tse23@gmail.com>
2022-03-27 01:58:35 -0700

    Complete 11_getTheTitles Exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let newArr = []
+    arr.forEach(book => newArr.push(book.title))
+    return newArr
 };
 
 // Do not edit below this line
