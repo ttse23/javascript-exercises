259aa91f7cd78756a8681f015ec3b10384a0dcb8
ttse23 <terence.tse23@gmail.com>
2022-03-27 01:34:58 -0700

    Complete 09_palindromes Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const LETTERS = "abcdefghijklmnopqrstuvwxyz"
+    indivLetters = str.toLowerCase().split("")
+    let alphaStr = indivLetters.filter(letter => LETTERS.includes(letter)).join("")
+    for (let i = 0; i < Math.floor(alphaStr.length / 2); i++){
+        if (alphaStr[i] !== alphaStr[alphaStr.length - 1 - i]){
+            return false
+        }
+        return true
+    }
 };
 
 // Do not edit below this line
