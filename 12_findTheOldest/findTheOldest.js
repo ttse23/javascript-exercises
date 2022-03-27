e553f6efe5240182c61e8cd9c701db4544f3bd35
ttse23 <terence.tse23@gmail.com>
2022-03-27 02:29:08 -0700

    Complete 12_findTheOldest Exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    arr.forEach(person => {
+        if (person.yearOfDeath === undefined){
+            person.yearOfDeath = new Date().getFullYear()
+        }
+    })
+    let oldest = arr.reduce((a, b) => {
+        let ageOfA = a.yearOfDeath - a.yearOfBirth
+        let ageOfB = b.yearOfDeath - b.yearOfBirth
+        if (ageOfA > ageOfB){
+            return a
+        } else{
+            return b
+        }
+    })
+    return oldest
 };
 
 // Do not edit below this line
